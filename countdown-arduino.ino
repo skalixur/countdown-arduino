ace3facb2c7f61fb92e6dbc8b758fece551f258f
Skalixur <jmjmbo5@gmail.com>
2025-07-10 03:18:36 +0800

    Countdown date formatter!

--- countdown-arduino.ino
+++ countdown-arduino.ino
@@ -6,7 +6,7 @@ RTClib rtc;
 
 void setup() {
   // Start the serial port
-  Serial.begin(57600);
+  Serial.begin(115200);
 
   // Start the I2C interface
   Wire.begin();
@@ -16,6 +16,31 @@ const uint32_t secondsInYear = 60UL * 60 * 24 * 365;
 const uint32_t secondsInDay = 60UL * 60 * 24;
 const uint16_t secondsInHour = 60 * 60;
 const byte secondsInMinute = 60;
+
+String pluralize(uint32_t count, String counter) {
+  return String(String(count) + ' ' + counter + (count == 1 ? "" : "s"));
+}
+
+void shift(String arr[], int& length) {
+  for (int i = 1; i < length; i++) arr[i - 1] = arr[i];
+  length--;
+}
+
+String dateArrayToString(String dateArray[], int length) {
+  while (dateArray[0].startsWith("0")) {
+    shift(dateArray, length);
+  }
+
+  String dateString;
+
+  for(int i = 0; i < length; i++) {
+    dateString += dateArray[i];
+    if(i == length - 1) continue;
+    dateString += ", " ;
+  }
+
+  return dateString;
+}
 
 String timeUntil(uint32_t dateAndTime) {
   // String dateAndTime: A date and time in the future as a Unix Timestamp, until the year ~2106
@@ -41,14 +66,17 @@ String timeUntil(uint32_t dateAndTime) {
   byte minutesToThen = duration / secondsInMinute;
   byte secondsToThen = duration % secondsInMinute;
 
-  String dateString =
-    String(yearsToThen) + " years, " + String(daysToThen) + " days, " + String(hoursToThen) + " hours, " + String(minutesToThen) + " minutes, " + String(secondsToThen) + " seconds until event";
+  int length = 5;
+  String dateArray[length] = { pluralize(yearsToThen, "year"), pluralize(daysToThen, "day"), pluralize(hoursToThen, "hour"), pluralize(minutesToThen, "minute"), pluralize(secondsToThen, "second") };
+
+  String dateString = dateArrayToString(dateArray, length);
 
   return dateString;
 }
 
 void loop() {
   Serial.println(timeUntil(countdownDate));
+  // Serial.println("hello");
 
   delay(1000);
 }
