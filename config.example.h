9a6183a1adc914748fe2bf5fb75bbfeb5ac984b8
Skalixur <jmjmbo5@gmail.com>
2025-07-10 02:37:49 +0800

    Add countdown calculator, edit example

--- config.example.h
+++ config.example.h
@@ -1,1 +1,2 @@
+// Set the date to count down to below, as a Unix timestamp, from now to sometime before the year ~2106
 uint32_t countdownDate = 1752082329;
--- countdown-arduino.ino
+++ countdown-arduino.ino
@@ -12,28 +12,43 @@ void setup() {
   Wire.begin();
 }
 
+const uint32_t secondsInYear = 60UL * 60 * 24 * 365;
+const uint32_t secondsInDay = 60UL * 60 * 24;
+const uint16_t secondsInHour = 60 * 60;
+const byte secondsInMinute = 60;
 
 String timeUntil(uint32_t dateAndTime) {
   // String dateAndTime: A date and time in the future as a Unix Timestamp, until the year ~2106
   // Example: 1752082329
-
   DateTime now = rtc.now();
   DateTime then = DateTime(dateAndTime);
-  unsigned long durationUntilThen = then.unixtime() - now.unixtime();
-  DateTime duration = DateTime(durationUntilThen);
 
-  unsigned int yearsUntil = duration.year();
-  unsigned int daysUntil = duration.day();
-  byte hoursUntil = duration.hour();
-  byte minutesUntil = duration.minute();
-  byte secondsUntil = duration.second();
+  if ((then.unixtime() - now.unixtime()) <= 0) {
+    return "EVENT COMPLETE";
+  }
 
-  String timeUntilString = "";
+  uint32_t duration = then.unixtime() - now.unixtime();
 
-  return String(durationUntilThen);
+  uint32_t yearsToThen = duration / secondsInYear;
+  duration %= secondsInYear;
+
+  uint16_t daysToThen = duration / secondsInDay;
+  duration %= secondsInDay;
+
+  byte hoursToThen = duration / secondsInHour;
+  duration %= secondsInHour;
+
+  byte minutesToThen = duration / secondsInMinute;
+  byte secondsToThen = duration % secondsInMinute;
+
+  String dateString =
+    String(yearsToThen) + " years, " + String(daysToThen) + " days, " + String(hoursToThen) + " hours, " + String(minutesToThen) + " minutes, " + String(secondsToThen) + " seconds until event";
+
+  return dateString;
 }
 
 void loop() {
   Serial.println(timeUntil(countdownDate));
+
   delay(1000);
 }
