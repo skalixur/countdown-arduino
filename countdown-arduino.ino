6f806913c7f6f13746512da2e036b38e8df46077
Skalixur <jmjmbo5@gmail.com>
2025-07-10 02:01:04 +0800

    Add basic RTC module code, create date retrieval

--- countdown-arduino.ino
+++ countdown-arduino.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DS3231.h>
+#include <Wire.h>
 
+RTClib rtc;
+
+void setup() {
+  // Start the serial port
+  Serial.begin(57600);
+
+  // Start the I2C interface
+  Wire.begin();
+}
+
+
+String timeUntil(int dateAndTime) {
+  // String dateAndTime: A date and time in the future as a Unix Timestamp
+  // Example: 1752082329
+
+  DateTime now = rtc.now();
+  DateTime then = DateTime(dateAndTime);
+  unsigned long durationUntilThen = then.unixtime() - now.unixtime();
+  DateTime duration = DateTime(durationUntilThen);
+
+  unsigned int yearsUntil = duration.year();
+  unsigned int daysUntil = duration.day();
+  byte hoursUntil = duration.hour();
+  byte minutesUntil = duration.minute();
+  byte secondsUntil = duration.second();
+
+  String timeUntilString = "";
+
+  return String(durationUntilThen);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println(timeUntil(1754496000));
+  delay(1000);
 }
